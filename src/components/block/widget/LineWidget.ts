6899b1bbbdea4a10dae0882ce84aade77cac23cf
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-06-01 00:14:39 +0300

    Add line chart to utils generate

--- src/components/block/widget/LineWidget.ts
+++ src/components/block/widget/LineWidget.ts
@@ -2,7 +2,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 import { CreateElement, VNode } from 'vue/types'
 
 import * as d3 from 'd3'
-import { generateAxis, generateData, generateGrid, generateLine, initLinePosition } from '@/utils/d3'
+import { AD3, generateAxis, generateData, generateGrid, generateLine, initLinePosition, ad3 } from '@/utils/d3'
 
 @Component({
   name: 'LineWidget'
@@ -29,120 +29,16 @@ export default class extends Vue {
     const node = this.$refs.svgChart as HTMLElement
     const height = node.parentElement ? node.parentElement.offsetHeight : 100
     const width = node.parentElement ? node.parentElement.offsetWidth : 100
-    let svg = d3.select(node as Element)
-      .attr('width', width)
-      .attr('height', height)
-      .append('g')
 
-    const data = generateData({ max: 18 })
-
-    const [x, xAxis] = generateAxis({
-      ticks: data.length < 15 ? data.length : 10,
-      domain: [0, data.length - 1],
-      range: [margin.left, width - margin.right],
-      translate: `translate(0, ${height - margin.bottom})`
-    })
-
-    const yTicks = data.length < 6 ? data.length : 6
-    const [y, yAxis] = generateAxis({
-      type: 'axisLeft',
-      ticks: yTicks,
-      domain: d3.extent(data, d => d.value) as [number, number],
-      domainOffset: 0.2,
-      range: [height - margin.bottom, margin.top],
-      translate: `translate(${margin.left}, 0)`
-    })
-
-    const grid = generateGrid({ width, height, margin, y, yLines: yTicks })
-    const callSvg = (func: (g: any) => any) => svg.append('g').call(func)
-
-    callSvg(xAxis)
-    callSvg(yAxis)
-    callSvg(grid)
-
-    const path: any = d3.line()
-      .x((_, i) => x(i))
-      .y((d: any) => y(d.value))
-      .curve(d3.curveCardinal)
-
-    const generatePicker = (g: any) => {
-      const size = 8
-
-      g.append('rect')
-        .attr('width', size)
-        .attr('height', size)
-        .attr('x', (size / 2) * -1)
-        .attr('y', (size / 2) * -1)
-        .attr('fill', 'var(--color-active)')
-        .attr('rx', size)
-        .attr('id', 'pointer')
-    }
-
-    svg.call(generatePicker)
-
-    const lineX = generateLine({
-      position: [margin.left, margin.left, margin.top, height - margin.bottom],
-      attrs: [['id', 'tooltip-line-x'], ['stroke', 'var(--color-active)'], ['stroke-opacity', '0.6'], ['opacity', '0']]
-    })
-    const lineY = generateLine({
-      position: [margin.left, width - margin.right, margin.top, margin.top],
-      attrs: [['id', 'tooltip-line-y'], ['stroke', 'var(--color-active)'], ['stroke-opacity', '0.6'], ['opacity', '0']]
-    })
-
-    svg.call(lineX)
-    svg.call(lineY)
-
-    svg.append('path')
-      .datum(data)
-      .attr('fill', 'none')
-      .attr('stroke', 'var(--color-active)')
-      .attr('stroke-width', 1.5)
-      .attr('d', path)
-
-    const bisect = (() => {
-      const bisect = d3.bisector((d: any) => d.index).right
-
-      return (mx: any) => {
-        const value = x.invert(mx)
-        const index = bisect(data, value, 1)
-
-        const a = data[index - 1]
-        const b = data[index]
-        return value - a.value > b.value - value ? b : a
+    const chart = new AD3(node, {
+      type: 'line',
+      data: generateData({ max: 18 }),
+      options: {
+        grid: true,
+        height,
+        width,
+        margin
       }
-    })()
-
-    const updateLinePosition = initLinePosition()
-    const body = svg.append('rect')
-      .attr('width', width - margin.left - margin.right)
-      .attr('height', height - margin.top - margin.bottom)
-      .attr('x', margin.left)
-      .attr('y', margin.top)
-      .attr('opacity', 0)
-
-    body.on('mouseenter', () => {
-      svg.selectAll('#pointer, #tooltip-line-x, #tooltip-line-y')
-        .transition()
-        .duration(200)
-        .style('opacity', 1)
-    }).on('mousemove', () => {
-      const { index, value } = bisect(d3.event.offsetX)
-      const position = { x: x(index), y: y(value) }
-
-      updateLinePosition({
-        animationCallback: (progress: number) => console.log({ progress }, 'progress'),
-        position,
-        duration: 400,
-        svg
-      })
-
-      svg.select('#pointer')
-        .attr('transform', `translate(${position.x}, ${position.y})`)
-    }).on('mouseleave', () => {
-      svg.selectAll('#pointer, #tooltip-line-x, #tooltip-line-y')
-        .transition()
-        .duration(200)
-        .style('opacity', 0)
     })
   }
 }
--- src/utils/d3/index.ts
+++ src/utils/d3/index.ts
@@ -1,7 +1,9 @@
+import * as d3 from 'd3'
+
 import { generateData } from './data'
 import { generateRadar } from './radar'
 import { generateAxis, AxisOptions } from './axis'
-import { generateLine, initLinePosition, generateGrid, GridOptions } from './line'
+import { generateLine, initLinePosition, generateGrid, GridOptions, IMargin } from './line'
 
 export { generateRadar, generateAxis, generateData, generateLine, initLinePosition, generateGrid }
 
@@ -10,17 +12,76 @@ type ChartTypes = 'line' | 'bar' | 'area' | 'radar' | 'pie' | 'arc' | 'chord'
 interface ConstructorAD3 {
   type: ChartTypes,
   data: any
-  options: {
-    grid?: GridOptions
-    axes: {
-      x: AxisOptions
-      y: AxisOptions
+  options?: {
+    width?: number
+    height?: number
+    margin?: IMargin
+    grid?: GridOptions | boolean
+    axes?: {
+      x?: AxisOptions
+      y?: AxisOptions
     }
   }
 }
 
-export class ad3 {
-  constructor(ctx: any, options: ConstructorAD3) {
-    console.log('asdf')
+export class AD3 {
+  constructor(ctx: any, settings: ConstructorAD3) {
+    const ctxCall = (func: (g: any) => any) => ctx.append('g').call(func)
+    const { type, data, options } = settings
+    let [width, height] = [ctx.clientWidth, ctx.clientHeight]
+    let margin = { top: 10, right: 10, bottom: 10, left: 10 }
+    let grid
+
+    if (options) {
+      grid = options.grid || false
+      width = options.width || width
+      height = options.height || height
+      margin = options.margin || margin
+    }
+
+    ctx = d3.select(ctx as Element)
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+
+    if (type === 'line') {
+      console.log({ data, width, height, margin, ctx })
+      const [x, xAxis] = generateAxis({
+        ticks: data.length < 15 ? data.length : 10,
+        domain: [0, data.length - 1],
+        range: [margin.left, width - margin.right],
+        translate: `translate(0, ${height - margin.bottom})`
+      })
+
+      const yTicks = data.length < 6 ? data.length : 6
+      const [y, yAxis] = generateAxis({
+        type: 'axisLeft',
+        ticks: yTicks,
+        domain: d3.extent(data, d => d.value) as [number, number],
+        domainOffset: 0.2,
+        range: [height - margin.bottom, margin.top],
+        translate: `translate(${margin.left}, 0)`
+      })
+
+      ctxCall(xAxis)
+      ctxCall(yAxis)
+
+      if (grid === true) {
+        const gridCall = generateGrid({ width, height, margin, y, yLines: yTicks })
+        ctxCall(gridCall)
+      }
+
+      const path: any = d3.line()
+        .x((_, i) => x(i))
+        .y((d: any) => y(d.value))
+        .curve(d3.curveCardinal)
+
+      ctx.append('path')
+        .datum(data)
+        .attr('fill', 'none')
+        .attr('stroke', 'var(--color-active)')
+        .attr('stroke-width', 1.5)
+        .attr('d', path)
+    }
   }
 }
