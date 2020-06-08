3d1330a87ab607c1e3f01f77b51e8eafab70cf32
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-06-08 23:47:30 +0300

    Add filter util

---
+++ src/utils/d3/filter.ts
@@ -0,0 +1,37 @@
+type FilterTypes = 'drop-shadow' | 'blur'
+
+export interface GenerateFilterOptions {
+  type: FilterTypes
+  color?: string
+  x?: number
+  y?: number
+  opacity?: number
+  blur?: number
+}
+
+export function generateFilter(options: GenerateFilterOptions) {
+  let { color, x, y, opacity, blur, type } = {
+    type: options.type,
+    color: options.color || 'white',
+    x: options.x || 0,
+    y: options.y || 0,
+    opacity: options.opacity || 1,
+    blur: options.blur || 10
+  }
+
+  const filter = (g: any) => {
+    const filter = g
+      .append('defs')
+      .append('filter')
+      .attr('id', 'drop-shadow')
+      .append('feDropShadow')
+      .attr('dy', y)
+      .attr('dx', x)
+      .attr('stdDeviation', blur)
+      .attr('flood-color', color)
+      .attr('flood-opacity', opacity)
+    console.log({ filter })
+  }
+
+  return filter
+}
